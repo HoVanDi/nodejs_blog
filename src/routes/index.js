7e151a92f75271404c8406f624b29f21118c7631
Di Ho <hovandidevelop@gmail.com>
2024-08-29 10:19:33 +0700

    course detail page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
+const courseRouter = require("./courses");
 const newsRouter = require("./news");
 const SiteRouter = require("./site");
 
 function route(app) {
   app.use("/news", newsRouter);
+  app.use("/course", courseRouter);
 
   app.use("/", SiteRouter);
 
