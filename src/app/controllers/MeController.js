6d8fa9c49b830b4b9836d406b64bf8760825fda1
HoVanDi <hovandideveloper@gmail.com>
2024-10-08 00:28:32 +0700

    feature/soft detele

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,11 +5,24 @@ const { mutipleMongooseToObject } = require("../../until/mongoose");
 class MeController {
   //[GET] me//stored/courses
   storedCourses(req, res, next) {
-    Course.find({ })
-    .then(courses => res.render("me/stored-courses" ,{
-      courses: mutipleMongooseToObject(courses),
-    }))
-    .catch(next);
+    Course.find({})
+      .then((courses) =>
+        res.render("me/stored-courses", {
+          courses: mutipleMongooseToObject(courses),
+        })
+      )
+      .catch(next);
+  }
+
+  //[GET] me//trash/courses
+  trashCourses(req, res, next) {
+    Course.findWithDeleted({ deleted: true })
+      .then((courses) =>
+        res.render("me/trash-courses", {
+          courses: mutipleMongooseToObject(courses),
+        })
+      )
+      .catch(next);
   }
 }
 
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -7,6 +7,9 @@ router.get("/create", courseController.create);
 router.post("/store", courseController.store);
 router.get("/:id/edit", courseController.edit);
 router.put("/:id", courseController.update);
+router.delete("/:id", courseController.destroy);
+router.delete("/:id/force", courseController.forceDestroy);
+router.patch("/:id/restore", courseController.restore);
 router.get("/:slug", courseController.show);
 
 module.exports = router;
